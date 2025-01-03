d757c69b7a6b487547f4e1ff2e5249d51ec21ffd
Meatfucker <tramshed@gmail.com>
2025-01-03 02:09:42 -0500

    Added create_tag and tag_assets functions

--- modules/api_client.py
+++ modules/api_client.py
@@ -67,7 +67,7 @@ class ImmichClient:
             self.asset_count['total'] = 0
             self.asset_count['images'] = 0
             self.asset_count['videos'] = 0
-            print(f"Error getting asset statistics. {e}")
+            print(f"Error getAssetStatistics API. {e}")
 
     def get_my_user(self):
         """Gets the users login"""
@@ -91,7 +91,7 @@ class ImmichClient:
         except Exception as e:
             self.user = "Unknown"
             self.logged_in = False
-            print(f"Error getting user. {e}")
+            print(f"Error accessing getMyUser API. {e}")
 
     def get_all_albums(self):
         """Returns a list of all album names and associated ids"""
@@ -108,9 +108,9 @@ class ImmichClient:
                 album_dict = {album['albumName']: album['id'] for album in albums}
                 return album_dict
             else:
-                print("Error accessing album API")
+                print("Error getting album list")
         except Exception as e:
-            print(f"Error getting album list: {e}")
+            print(f"Error accessing getAllAlbums API: {e}")
 
     def create_album(self, album_name):
         """Creates an album"""
@@ -133,11 +133,33 @@ class ImmichClient:
             else:
                 print(f"Error creating album {album_name}")
         except Exception as e:
-            print(f"Error accessing album API: {e}")
+            print(f"Error accessing createAlbum API: {e}")
 
-    def add_assets_to_album(self, album_id, asset_ids):
+    def create_tag(self, tag_name):
+        """Creates an album"""
+        url = f"{self.base_url}/api/tags"
+        payload = json.dumps({
+            'Name': tag_name,
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'x-api-key': self.token
+        }
+        try:
+            response = requests.request("POST", url, headers=headers, data=payload)
+            if response.status_code == 201:
+                response_data = response.json()
+                album_id = response_data.get('id')  # Extract the 'id' field
+                return album_id
+            else:
+                print(f"Error creating tag {tag_name}")
+        except Exception as e:
+            print(f"Error accessing createTag API: {e}")
+
+    def tag_assets(self, tag_id, asset_ids):
         """Takes an album id and a list of asset ids then adds them to the album"""
-        url = f"{self.base_url}/api/albums/{album_id}/assets"
+        url = f"{self.base_url}/api/tags/{tag_id}/assets"
         payload = json.dumps({
             'ids': asset_ids
         })
@@ -149,9 +171,9 @@ class ImmichClient:
         try:
             response = requests.request("PUT", url, headers=headers, data=payload)
             if response.status_code != 200:
-                print(f"Error adding assets to album {album_id}")
+                print(f"Error tagging assets with tag: {tag_id}")
         except Exception as e:
-            print(f"Error accessing addAssetsToAlbum API: {e}")
+            print(f"Error accessing tagAssets API: {e}")
 
     def upload_asset(self, file):
         """Uploads a single asset, hashing an AssetId for it. Returning the immich id and upload status"""
@@ -185,7 +207,7 @@ class ImmichClient:
                 else:
                     print(f'Error uploading {file}')
             except Exception as e:
-                print(f'Error accessing upload API: {e}')
+                print(f'Error accessing uploadAsset API: {e}')
             # Retry logic: wait before the next attempt
             attempt += 1
             if attempt < 3:
